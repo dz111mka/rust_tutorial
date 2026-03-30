c874ee834da238855dbb9b23931603b03496a778
dima <chepikov.d.u@gmail.com>
2026-03-30 22:58:45 +0300

    фиксы

--- src/bin/fibonacci_generator.rs
+++ src/bin/fibonacci_generator.rs
@@ -1,26 +1,4 @@
 #![warn(clippy::all, clippy::pedantic)]
-
-/*use std::io;
-
-fn main() {
-    let mut n = String::new();
-    io::stdin().read_line(&mut n).expect("Failed to read line");
-
-    let n: i128 = n.trim().parse().expect("Нужно ввести число");
-
-    let i = fibonacci_generator(n);
-    println!("Число Фибоначчи для коэффициента {n} равно {i}");
-}
-
-fn fibonacci_generator(n: i128) -> i128 {
-    if n == 0 {
-        return 0;
-    } else if n == 1 {
-        return 1;
-    }
-    fibonacci_generator(n - 1) + fibonacci_generator(n - 2)
-}*/
-
 // вариант предложенный DeepSeek
 use std::io;
 
@@ -58,3 +36,26 @@ fn fibonacci_generator(n: i128) -> i128 {
         }
     }
 }
+
+
+/*use std::io;
+
+fn main() {
+    let mut n = String::new();
+    io::stdin().read_line(&mut n).expect("Failed to read line");
+
+    let n: i128 = n.trim().parse().expect("Нужно ввести число");
+
+    let i = fibonacci_generator(n);
+    println!("Число Фибоначчи для коэффициента {n} равно {i}");
+}
+
+fn fibonacci_generator(n: i128) -> i128 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    }
+    fibonacci_generator(n - 1) + fibonacci_generator(n - 2)
+}*/
+
